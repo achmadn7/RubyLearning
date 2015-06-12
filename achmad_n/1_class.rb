ee718ea3fa2c2f8ddc91994cb14be794914a8022
antasa7 <achmad1300018083@webmail.uad.ac.id>
2015-06-12 20:37:10 +0700

    Selesai Memodifikasi 1_class.rb yg isinya class dan object

---
+++ achmad_n/1_class.rb
@@ -0,0 +1,10 @@
+#contoh perintah membuat class
+class Mencoba
+   def an
+      puts "Semangat Belajar Ruby!"
+   end
+end
+
+#contoh perintah membuat objek dari class
+object = Mencoba. new
+object.an
