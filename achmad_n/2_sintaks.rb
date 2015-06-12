b34873372832ece353f93dec3abcf81153d5f0a5
antasa7 <achmad1300018083@webmail.uad.ac.id>
2015-06-12 20:22:44 +0700

    Memodifikasi File 2_sintaks

---
+++ achmad_n/2_sintaks.rb
@@ -0,0 +1,16 @@
+#contoh perintah untuk string AN
+print <<AN
+    Ini adalah contohnya.
+AN
+
+#contoh perintah eksekusi 
+print <<`A`     
+	echo hai an
+A
+
+#contoh perintah tumpukan
+print <<"atas", <<"bawah"  
+	Atas.
+atas
+	Bawah.
+bawah
