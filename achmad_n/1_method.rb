2d2be6443000c43f7be6c1db94589d85b286d8b3
antasa7 <achmad1300018083@webmail.uad.ac.id>
2015-06-13 15:55:26 +0700

    Memodifikasi FIle 1_method.rb

---
+++ achmad_n/1_method.rb
@@ -0,0 +1,7 @@
+#contoh perintah method
+def nama_mobil(a1="Lancer", a2="skyline")
+   puts "Ini adalah mobil #{a1}"
+   puts "Ini adalah mobil #{a2}"
+end
+nama_mobil "Lamborghini", "Porsche"
+nama_mobil
